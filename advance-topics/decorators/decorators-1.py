35861c2842a1baa0578aeaa00bad26108246d042
Ashwani Singh <ashsingh16@publicisgroupe.net>
2020-10-22 17:22:10 +0530

    python decorators

---
+++ advance-topics/decorators/decorators-1.py
@@ -0,0 +1,20 @@
+def smart_divide(func):
+    def inner(a, b):
+        print("I am going to divide", a, "and", b)
+        if b == 0:
+            print("Whoops! cannot divide")
+            return
+
+        return func(a, b)
+    return inner
+
+
+@smart_divide
+def divide(a, b):
+    print(a/b)
+
+divide(4,6)
+
+divide(3,0)
+
+divide = smart_divide(divide(3,0))
---
+++ advance-topics/decorators/decorators-2.py
@@ -0,0 +1,27 @@
+def star(func):
+    def inner(*args, **kwargs):
+        print("*" * 30)
+        func(*args, **kwargs)
+        print("*" * 30)
+    return inner
+
+
+def percent(func):
+    def inner(*args, **kwargs):
+        print("%" * 30)
+        func(*args, **kwargs)
+        print("%" * 30)
+    return inner
+
+
+@star
+@percent
+def printer(msg):
+    print(msg)
+
+
+printer("Hello! Ashwani Singh") 
+
+# printer = star(percent(printer))
+# test = star(percent(printer))
+# test("hello! Ashwani Singh")
